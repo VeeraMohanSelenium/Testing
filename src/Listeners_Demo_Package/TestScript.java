3e9d2b681acff6806f9609694365b3f0622fbc26
SRINIVAS <SRINIVAS@192.168.0.21>
2018-02-02 12:43:47 +0530

    1st commit

---
+++ src/Listeners_Demo_Package/TestScript.java
@@ -0,0 +1,35 @@
+package Listeners_Demo_Package;
+
+import org.openqa.selenium.By;		
+import org.openqa.selenium.WebDriver;		
+import org.openqa.selenium.firefox.FirefoxDriver;		
+import org.testng.Assert;		
+import org.testng.annotations.Listeners;		
+import org.testng.annotations.Test;	
+
+//One of the way to use Listeners annotation (@Listeners) 
+
+@Listeners(Listeners_Demo_Package.ListenerTest.class)
+
+public class TestScript 
+{
+	WebDriver driver= new FirefoxDriver();					
+
+	// Test to pass as to verify listeners .		
+	@Test		
+	public void Login()				
+	{		
+	    driver.get("http://demo.guru99.com/V4/");					
+	    driver.findElement(By.name("uid")).sendKeys("mngr34926");							
+	    driver.findElement(By.name("password")).sendKeys("amUpenu");							
+	    driver.findElement(By.name("btnLogin")).click();					
+	}		
+
+	// Forcefully failed this test as to verify listener.		
+	@Test		
+	public void TestToFail()				
+	{		
+	    System.out.println("This method to test fail");					
+	    Assert.assertTrue(false);			
+	}		
+}
